cd9f2d4b390294ee23198c221f92aa8a5d314cfa
Xinyao(Alvin) Sun <xinyao1@ualberta.ca>
2022-03-01 13:27:28 -0700

    Update opener.ts

--- src/util/opener.ts
+++ src/util/opener.ts
@@ -11,14 +11,15 @@ module.exports = function opener(
   let platform = process.platform
   args = [].concat(args)
 
+  // WSL2 with WSLg 
   // Attempt to detect Windows Subystem for Linux (WSL).
   // WSL  itself as Linux (which works in most cases), but in
   // this specific case we need to treat it as actually being Windows.
   // The "Windows-way" of opening things through cmd.exe works just fine here,
   // whereas using xdg-open does not, since there is no X Windows in WSL.
-  if (platform === 'linux' && os.release().toLowerCase().indexOf('microsoft') !== -1) {
-    platform = 'win32'
-  }
+  // if (platform === 'linux' && os.release().toLowerCase().indexOf('microsoft') !== -1) {
+  //   platform = 'win32'
+  //}
 
   // http://stackoverflow.com/q/1480971/3191, but see below for Windows.
   let command
